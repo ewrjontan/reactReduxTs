f3996195edec89cd69b79aec6b55e167553bc185
Jonathan Cardenas <jonscardenas@gmail.com>
2021-12-10 13:34:48 -0600

    project completed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
     todos: Todo[];
-    fetchTodos(): any;
+    fetchTodos: Function;//no way to tell react-redux what type redux-thunk is
+    deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+    fetching: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { fetching: false };
+    }
+
+    componentDidUpdate(prevProps: AppProps): void {
+        if (!prevProps.todos.length && this.props.todos.length)
+        this.setState({ fetching: false })
+    }
+
     onButtonClick = (): void => {
         this.props.fetchTodos();
+        this.setState({ fetching: true });
     };
+
+    onTodoClick = (id: number): void => {
+        this.props.deleteTodo(id)
+    }
 
     renderList(): JSX.Element[] {
             return this.props.todos.map((todo: Todo) => {
-                return <div key={todo.id}>{todo.title}</div>
+                return (
+                    <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+                        {todo.title}
+                    </div>
+                )
             });
     }
 
@@ -26,6 +50,7 @@ class _App extends React.Component<AppProps> {
         return ( 
             <div>
                 <button onClick={this.onButtonClick}>Fetch</button>
+                {this.state.fetching ? 'Loading': null}
                 {this.renderList()}
 
             </div>
@@ -37,4 +62,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[]} => {
     return { todos };
 }
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
