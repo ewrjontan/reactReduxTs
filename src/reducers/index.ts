3ffc8acf637db3e9d7df8742f55014e442cded1a
Jonathan Cardenas <jonscardenas@gmail.com>
2021-12-10 11:12:47 -0600

    added reducer and todos

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
+
+export interface StoreState {
+    todos: Todo[];
+}
 
 //need atleast one reducer to avoid an error
-export const reducers = combineReducers({
-    counter: () => 1
+export const reducers = combineReducers<StoreState>({
+    todos: todosReducer
 });
